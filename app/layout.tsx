3d522bedd12f552966c4ce261f6159c915c82e13
Tung Pham <tung.42@gmail.com>
2025-09-19 13:16:35 +0700

    og:title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Image Converter",
-  description:
-    "An image converter application built with Next.js and Tailwind CSS.",
-};
 
 export default function RootLayout({
   children,
@@ -28,10 +21,8 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
       </head>
-      <body className={inter.className}>
-        {children}
-        <GoogleAnalytics ga_id="G-HHXZSNQ65X" />
-      </body>
+      <body className={inter.className}>{children}</body>
+      <GoogleAnalytics ga_id="G-HHXZSNQ65X" />
     </html>
   );
 }
